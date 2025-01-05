9fa54c5f64354265dc26dc7a59f4d450b17bde00
dev <azertwxcvbn259@gmail.com>
2025-01-05 22:17:10 +0100

    création bda + écriture des requêtes sql pour dynamiser le site

--- variables.php
+++ variables.php
@@ -1,50 +1,13 @@
 <?php
-$users = [
-    [
-        'full_name' => 'Mickaël Andrieu',
-        'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34,
-        'password' => '1234',
-    ],
-    [
-        'full_name' => 'Mathieu Nebra',
-        'email' => 'mathieu.nebra@exemple.com',
-        'age' => 34,
-        'password' => '5678',
-    ],
-    [
-        'full_name' => 'Laurène Castor',
-        'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-        'password' => '91011',
-    ],
-];
 
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => 'Etape 1 : des flageolets !',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => 'Etape 1 : de la semoule',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => 'Etape 1 : prenez une belle escalope',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Salade Romaine',
-        'recipe' => 'Etape 1 : prenez une belle salade',
-        'author' => 'laurene.castor@exemple.com',
-        'is_enabled' => false,
-    ],
-];
+require_once(__DIR__ . '/databaseconnect.php');
+
+$usersStatement = $mysqlClient->prepare('SELECT * FROM users');
+$usersStatement->execute();
+$users = $usersStatement->fetchAll();
+
+$recipesStatement = $mysqlClient->prepare('SELECT * FROM recipes WHERE is_enabled is TRUE');
+$recipesStatement->execute();
+$recipes = $recipesStatement->fetchAll();
 
 ?>
